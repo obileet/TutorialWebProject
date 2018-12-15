83e568484cd5ef0879fdd4098fcd0ffe21659121
obilee <obileet@gmail.com>
2018-12-15 18:47:26 +0100

    menu color switch leistungen

--- InGlory/js/plugins.js
+++ InGlory/js/plugins.js
@@ -12874,12 +12874,14 @@ $(function () {
             offset: -120
         }).addTo(l).on("enter", function (o) {
             $(".logo").removeClass("logo-light"), $(".navigation-icon .line").addClass("line-dark"),
-            $(".logoImpressum").removeClass("logo-dark").addClass("logo-light"),
-            $(".impressumLine").addClass("line").removeClass("menuline-dark");
+                $(".logoImpressum").removeClass("logo-dark").addClass("logo-light"),
+                $(".impressumLine").addClass("line").removeClass("menuline-dark"),
+                $(".indexLine").addClass("menuline-dark").removeClass("line");
         }).on("leave", function (o) {
             $(".logo").addClass("logo-light"), $(".navigation-icon .line").removeClass("line-dark"),
                 $(".logoImpressum").removeClass("logo-light").addClass("logo-dark"),
-                $(".impressumLine").addClass("menuline-dark").removeClass("line");;
+                $(".impressumLine").addClass("menuline-dark").removeClass("line"),
+                $(".indexLine").addClass("line").removeClass("menuline-dark");
         });
         $(window).on("resize", function () {
             t.duration(n.outerHeight())
