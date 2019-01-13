8a41f42274d795bdde332ff94c66e30b4ea1bc39
Mike <mail@inglory.de>
2019-01-13 17:33:41 +0100

    Font Cleanup - Copyright Cleanup

--- InGlory/contact.php
+++ InGlory/contact.php
@@ -1,8 +1,8 @@
 ﻿<?php
 $errors  = '';
-$myemail = 'mail@inglory.de';
+$myemail = 'info@inglory.de';
 if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['message'])) {
-    $errors .= "\n Fehler: Bitte Füllen Sie diese Feld aus";
+    $errors .= "\n Error: Required Field";
 }
 
 $name    = $_POST['name'];
@@ -11,7 +11,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)) {
-    $errors .= "\n Fehler: Keine gültige E-Mail Adresse";
+    $errors .= "\n Error: Invalid Email Address";
 }
 
 if (empty($errors)) {
@@ -19,7 +19,7 @@ if (empty($errors)) {
     $email_subject = "$subject";
     $email_body    = "\n Name: $name \n Email: $email \n Subject: $subject \n Message: \n $message";
     $headers       = "From: $email";
-    
-    @mail($to, $email_subject, $email_body, $headers);
+
+    mail($to, $email_subject, $email_body, $headers);
 }
 ?>
